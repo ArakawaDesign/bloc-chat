80041c3e32b8afc67b7e0f08954af1e17c6eb1f8
Ryan Arakawa <arakawa.ryan@gmail.com>
2017-02-01 16:16:25 -0600

    commit before git pull

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,8 +1,5 @@
 (function() {
     function Room($firebaseArray) {
-        console.log(ref);
-        console.log($firebaseArray);
-        console.log(rooms);
         var ref = firebase.database().ref().child("rooms");
         var rooms = $firebaseArray(ref);
     
